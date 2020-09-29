a981aabfd0fb4a4c61b9506308358d9908fd9537
Kevin Terwelp <kterwelp@me.com>
2020-09-29 11:16:00 -0500

    Initial Commit

---
+++ COSC4355_Medication_Reminder_Project/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  COSC4355_Medication_Reminder_Project
+//
+//  Created by Kevin Terwelp on 9/29/20.
+//  Copyright © 2020 Kevin Terwelp. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ COSC4355_Medication_Reminder_ProjectTests/COSC4355_Medication_Reminder_ProjectTests.swift
@@ -0,0 +1,34 @@
+//
+//  COSC4355_Medication_Reminder_ProjectTests.swift
+//  COSC4355_Medication_Reminder_ProjectTests
+//
+//  Created by Kevin Terwelp on 9/29/20.
+//  Copyright © 2020 Kevin Terwelp. All rights reserved.
+//
+
+import XCTest
+@testable import COSC4355_Medication_Reminder_Project
+
+class COSC4355_Medication_Reminder_ProjectTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
