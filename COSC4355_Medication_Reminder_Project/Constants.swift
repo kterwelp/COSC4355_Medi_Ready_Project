22a83671908122a6cf467d782ddbf1458d5f7197
jaredtalbert <jjtalbert@uh.edu>
2020-10-20 01:25:39 -0500

    Add Constants file

---
+++ COSC4355_Medication_Reminder_Project/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  COSC4355_Medication_Reminder_Project
+//
+//  Created by Jared Talbert on 10/20/20.
+//  Copyright © 2020 Kevin Terwelp. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Constants {
+    struct TableView {
+        static let CellHeight: CGFloat = 60
+    }
+}
