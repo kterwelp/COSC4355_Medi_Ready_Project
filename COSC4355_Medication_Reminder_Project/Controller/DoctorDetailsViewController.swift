8e1c14c8a15469de96dc0785e789f8144088246e
Kevin Terwelp <kterwelp@me.com>
2020-10-20 00:19:43 -0500

    Added buttons for adding doctor and viewing profile Added Doctor Information (Doctor Details) Screen with buttons to Edit and Delete (Buttons have no functionality in this version)

---
+++ COSC4355_Medication_Reminder_Project/Controller/DoctorDetailsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  DoctorDetailsViewController.swift
+//  COSC4355_Medication_Reminder_Project
+//
+//  Created by Kevin Terwelp on 10/19/20.
+//  Copyright © 2020 Kevin Terwelp. All rights reserved.
+//
+
+import UIKit
+
+class DoctorDetailsViewController: UIViewController {
+    
+    var passedInformation = Doctor()
+
+    @IBOutlet weak var doctorNameLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        doctorNameLabel.text = passedInformation.name
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
